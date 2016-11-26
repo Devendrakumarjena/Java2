c69103139d7317416ef18b0ab43edc78c65efda5
Devendra kumar jena <devendrajenatrichy@gmail.com>
2016-11-26 12:17:38 +0530

    nestedwhile

--- NestedWhile.java
+++ NestedWhile.java
@@ -4,15 +4,17 @@ public class NestedWhile {
 
 	public static void main(String[] args) {
 		int a=1;
-		int b=1;
+		
 		while(a<=5)
 		{
-			while(b<=5)
+			int b=5;
+			while(b>0)
 			{
-				System.out.println("value of b : "+b);
-				b++;
+				
+				System.out.print(b);
+				b--;
 			}
-			System.out.println("value of a : "+a);
+			System.out.println("  value of a : "+a);
 			a++;
 		}
 
