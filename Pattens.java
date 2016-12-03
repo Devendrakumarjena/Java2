08604d879a5afaa3b813299f97bd2b58c90d9259
Devendra kumar jena <devendrajenatrichy@gmail.com>
2016-12-03 09:21:14 +0530

    pattens

---
+++ Pattens.java
@@ -0,0 +1,60 @@
+package Practice;
+
+public class Pattens {
+
+	public static void main(String[] args) {
+		
+		/* output
+		     *
+		    ***
+		   *****
+		  *******
+		   *****
+		    ***
+		     *
+		 */
+		
+		int noOfcol=1;
+		int noofs=3;
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=1;j<=noofs;j++)
+				{
+					System.out.print(" ");
+					
+			}
+		
+		if(i<4){
+			noofs=noofs-1;
+		}
+		else 
+		{
+			noofs=noofs+1;
+			
+		}
+			for(int j=1;j<=noOfcol;j++)
+			{
+				
+				System.out.print("");
+			
+				System.out.print("*");
+				
+			}
+			System.out.println();
+			if(i<4)
+			{
+			noOfcol=noOfcol+2;
+			}
+			else
+			{
+				noOfcol=noOfcol-2;
+			}
+			
+			
+		}
+			
+		
+
+	}
+
+}
