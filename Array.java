fe9935119bd0034968a615fd13a1c87c13daaadc
Devendra kumar jena <devendrajenatrichy@gmail.com>
2016-12-09 10:25:42 +0530

    Array example

---
+++ Array.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int No[]={45,34,65,23,47,59};
+		int c= No[3]+No[5];
+		System.out.println(No[2]);
+		System.out.println(c);
+
+	}
+
+}
