b69dac53250e104b84c3fcdac338511d0f288f69
Devendra kumar jena <devendrajenatrichy@gmail.com>
2016-12-30 13:15:58 +0530

    Interface example

---
+++ Nokia950.java
@@ -0,0 +1,14 @@
+package Home.practice;
+
+public class Nokia950 extends Nokia {
+
+	public static void main(String[] args) {
+	
+		Nokia obj1=new Nokia950();
+		obj1.call();
+		obj1.msg();
+		obj1.whatup();
+
+	}
+
+}
