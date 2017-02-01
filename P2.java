461e42e46a73fb9b17e7d3bac4ef518ba17f971d
Devendra kumar jena <devendrajenatrichy@gmail.com>
2017-02-01 15:55:21 +0530

    array

---
+++ P2.java
@@ -0,0 +1,15 @@
+package arrayPractice;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		int[] no={2,5,3,5,6,7,2};
+		int sum=0;
+		for (int i=0; i<=5;i++)//to add parcticular no in arrays
+			sum +=i;
+		System.out.println("Ans : "+sum);
+		
+
+	}
+
+}
